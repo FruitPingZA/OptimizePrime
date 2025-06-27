b0cbe12fe36ba4aaf28adfa00ebc701b62db7245
FruitPingZA <shane94dev@gmail.com>
2025-06-27 11:02:48 +0200

    Update svgConverter.js
    Signed-off-by: FruitPingZA <shane94dev@gmail.com>

--- utils/svgConverter.js
+++ utils/svgConverter.js
@@ -1,19 +1,24 @@
 function textToSVG(text, font = "Arial", size = 48, color = "#ffffff") {
   return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="500" height="100">
-      <text x="0" y="${size}" font-family="${font}" font-size="${size}" fill="${color}">${text}</text>
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width="500" height="100">
+      <text x="0" y="${size}"
+            font-family="${font}"
+            font-size="${size}"
+            fill="${color}">${text}</text>
     </svg>
   `.trim();
 }
 
 function canvasToSVG(canvas) {
-  const svg = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="${canvas.width}" height="${canvas.height}">
-      <image href="${canvas.toDataURL("image/png")}" width="${canvas.width}" height="${canvas.height}" />
+  return `
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width="${canvas.width}" height="${canvas.height}">
+      <image href="${canvas.toDataURL("image/png")}"
+             width="${canvas.width}"
+             height="${canvas.height}" />
     </svg>
   `.trim();
-  return svg;
 }
 
-// Expose to global scope for script.js
 window.textToSVG = textToSVG;
