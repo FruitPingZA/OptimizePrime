360a19cf20c95c47a5b39aca81927782ca3de3e3
FruitPingZA <shane94dev@gmail.com>
2025-07-03 13:10:23 +0200

    Update svgConverter.js
    Signed-off-by: FruitPingZA <shane94dev@gmail.com>

--- utils/svgConverter.js
+++ utils/svgConverter.js
@@ -1,17 +1,22 @@
-// utils/svgConverter.js
-
-export function textToSVG(text, font = "Arial", color = "#000", css = "") {
-  const escapedText = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-  const svg = `
-<svg xmlns="http://www.w3.org/2000/svg" width="500" height="100">
+export function textToSVG(text, font = "Arial", color = "#000000") {
+  return `
+<svg xmlns="http://www.w3.org/2000/svg" width="300" height="100">
   <style>
-    text {
-      font-family: '${font}';
-      fill: ${color};
-    }
-    ${css}
+    text { font-family: '${font}'; fill: ${color}; font-size: 32px; }
   </style>
-  <text x="10" y="50" font-size="40">${escapedText}</text>
-</svg>`;
-  return svg.trim();
+  <text x="10" y="50">${text}</text>
+</svg>
+`.trim();
+}
+
+export function applyCustomCSSToSVG(svgContent, customCSS) {
+  const insertIndex = svgContent.indexOf("</style>");
+  if (insertIndex !== -1) {
+    const updatedSVG =
+      svgContent.slice(0, insertIndex) +
+      `\n${customCSS}\n` +
+      svgContent.slice(insertIndex);
+    return updatedSVG;
+  }
+  return svgContent;
 }
