2d99e6901029b157f21c3a945e03287fecd64be4
FruitPingZA <shane94dev@gmail.com>
2025-07-14 12:56:21 +0200

    Update webp_wrapper.js
    Signed-off-by: FruitPingZA <shane94dev@gmail.com>

--- codecs/webp/webp_wrapper.js
+++ codecs/webp/webp_wrapper.js
@@ -9,8 +9,16 @@ function getModule() {
   return modulePromise;
 }
 
-export async function encode(data, width, height, options) {
+/**
+ * Encodes raw RGBA data to WebP using Squoosh's WASM module.
+ * @param {Uint8ClampedArray} data - RGBA pixel data.
+ * @param {number} width
+ * @param {number} height
+ * @param {object} options - { quality: 0-100 }
+ */
+export async function encode(data, width, height, options = { quality: 80 }) {
   const module = await getModule();
   await module.ready;
+  // Squoosh expects options.quality
   return module.encode(data, width, height, options);
 }
