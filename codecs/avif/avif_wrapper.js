36ce94f7a3433a85ddd6f4bed4a5863376faacd2
FruitPingZA <shane94dev@gmail.com>
2025-07-14 12:54:55 +0200

    Update avif_wrapper.js
    Signed-off-by: FruitPingZA <shane94dev@gmail.com>

--- codecs/avif/avif_wrapper.js
+++ codecs/avif/avif_wrapper.js
@@ -9,8 +9,16 @@ function getModule() {
   return modulePromise;
 }
 
-export async function encode(data, width, height, options) {
+/**
+ * Encodes raw RGBA data to AVIF using Squoosh's WASM module.
+ * @param {Uint8ClampedArray} data - RGBA pixel data.
+ * @param {number} width
+ * @param {number} height
+ * @param {object} options - { quality: 0-100, speed: 0-10 }
+ */
+export async function encode(data, width, height, options = { quality: 80, speed: 6 }) {
   const module = await getModule();
   await module.ready;
+  // Squoosh expects options.quality and options.speed
   return module.encode(data, width, height, options);
 }
