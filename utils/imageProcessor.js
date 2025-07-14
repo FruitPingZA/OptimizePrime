3d34f1fff09faf591a54878bd70b0c702547792b
FruitPingZA <shane94dev@gmail.com>
2025-07-14 13:06:49 +0200

    Update imageProcessor.js
    Signed-off-by: FruitPingZA <shane94dev@gmail.com>

--- utils/imageProcessor.js
+++ utils/imageProcessor.js
@@ -34,8 +34,13 @@ export async function compressImage(file, format, maxWidth, maxHeight, targetSiz
     blob = new Blob([encoded.buffer], { type: "image/avif" });
   } else if (format === "webp") {
     const imageData = ctx.getImageData(0, 0, newWidth, newHeight);
-    // Squoosh WebP expects { quality: 0-100, lossless: boolean, qualityAlpha: 0-100 }
-    const webpOptions = { quality: quality, lossless: false, qualityAlpha: quality };
+    // Squoosh WebP expects { quality, lossless, qualityAlpha, method }
+    const webpOptions = {
+      quality: quality,
+      lossless: false,
+      qualityAlpha: quality,
+      method: 4
+    };
     const encoded = await encodeWebp(imageData.data, newWidth, newHeight, webpOptions);
     blob = new Blob([encoded.buffer], { type: "image/webp" });
   } else {
